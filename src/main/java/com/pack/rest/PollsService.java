abf2ddc3865bc97351f8d2eee7731d97fea3094f
LAPTOP-MDUDRMJD\Mato <matusbodorik1994@gmail.com>
2018-05-21 16:37:24 +0200

    "REST API Client java aplication"

--- src/main/java/com/pack/rest/PollsService.java
+++ src/main/java/com/pack/rest/PollsService.java
@@ -5,27 +5,36 @@ import retrofit2.Call;
 import retrofit2.http.*;
 
 public interface PollsService {
-    @GET("/api/users?page=id")
-    Call<UsersPage> getUserPage(@Query("id") Integer id); //@Query("id") Integer id
+    @POST("/user")
+    Call<User> postNewUser(@Body User user);
 
-    @GET("/api/users/{id}")
-    Call<User> getUser(@Path("id") Integer id);
+    @GET("/user")
+    Call<User> getUsers();
 
-    @GET("/api/unknown")
-    Call<UnknowPage> getUnknowPage();
+    @POST("/data")
+    Call<Sensor> postNewData(@Body Sensor sensor);
 
-    @GET("/api/unknown/{id}")
-    Call<Unknow> getUnknowData(@Path("id") Integer id);
+    @POST("/device")
+    Call<Device> postNewDevice(@Body Device device);
 
-    @POST("/api/users")
-    Call<NewUser> postNewUser(@Body NewUser newuser);
-
-    @PUT("/api/users/2")
-    Call<NewUser> putNewUser(@Body NewUser newuser);
-
-    @PATCH("/api/users/2")
-    Call<NewUser> patchNewUser(@Body NewUser newuser);
-
-    @DELETE("/api/users/2")
-    Call<NewUser> DeleteUser();
+//    @GET("/api/users/{id}")
+//    Call<User> getUser(@Path("id") Integer id);
+//
+//    @GET("/api/unknown")
+//    Call<UnknowPage> getUnknowPage();
+//
+//    @GET("/api/unknown/{id}")
+//    Call<Unknow> getUnknowData(@Path("id") Integer id);
+//
+//    @POST("/user")
+//    Call<NewUser> postNewUser(@Body NewUser newuser);
+//
+//    @PUT("/api/users/2")
+//    Call<NewUser> putNewUser(@Body NewUser newuser);
+//
+//    @PATCH("/api/users/2")
+//    Call<NewUser> patchNewUser(@Body NewUser newuser);
+//
+//    @DELETE("/api/users/2")
+//    Call<NewUser> DeleteUser();
 }
--- src/main/java/com/pack/rest/User.java
+++ src/main/java/com/pack/rest/User.java
@@ -3,16 +3,63 @@ package com.pack.rest;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class User {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    @SerializedName("data")
+class User {
+
+    @SerializedName("name")
     @Expose
-    public DataUser data;
+    public String name;
+    @SerializedName("password")
+    @Expose
+    public String password;
+    @SerializedName("role")
+    @Expose
+    public String role;
 
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this).append("data", data).toString();
+   /**
+    * Konstruktor
+    * @param name uzivatelske meno
+    * @param password uzivatelske heslo
+    * @param role uzivatelske prava
+    */
+
+   public User(String name, String password, String role){
+      this.name = name;
+      this.password= password;
+      this.role = role;
+   }
+
+   /**
+    * Metoda, ktora vykona POST poziadavku na server s uzivatelskymi datami pre
+    * pridanie Usera
+    * @param service - service
+    */
+    public void addUser(PollsService service) {
+       service.postNewUser(this).enqueue(new Callback<User>() {
+          public void onResponse(Call<User> call, Response<User> response) {
+             Logger.getGlobal().log(Level.INFO, "" + response.code());
+             if(response.code() == 200) {
+                 Logger.getGlobal().log(Level.INFO, response.body().toString());
+             }
+          }
+          public void onFailure(Call<User> call, Throwable throwable) {
+             throw new UnsupportedOperationException("Error POST new User!");
+          }
+       });
     }
 
+   public void getAllUsers(PollsService service){
+
+   }
+
+   @Override
+   public String toString() {
+      return new ToStringBuilder(this).append("name", name).append("password", password).append("role", role).toString();
+   }
 }
